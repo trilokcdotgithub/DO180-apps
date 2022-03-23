4f0b49a568c7a4fb9dc2bf0bc2bd1343c23b7839
Student User <student@workstation.lab.example.com>
2022-03-23 00:30:09 -0400

    Changed index page contents.

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,4 @@
 <?php
-print "Hello, World! php version is " . PHP_VERSION . "\n";
+print "Hello, C-DOT! php version is " . PHP_VERSION . "\n";
+print "C-DOT Hello, C-DOT! php version is " . PHP_VERSION . "\n";
 ?>
